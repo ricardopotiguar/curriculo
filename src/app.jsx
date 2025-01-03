7b8d99a5dd5f0b6c0b07e119563f1a58ec437530
Ricardo Potiguar <ricardopotiguar@gmail.com>
2025-01-03 14:46:04 -0300

    Exclusão de páginas desnecessárias. login, donations

--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home'
 import CadastroUsuario from './pages/cadastroUsuario';
 import Success from './pages/cadastroUsuario/success';
-import Login from './pages/login';
-import PrivateRoute from './components/privateRoute/privateRoute';
-import Donations from './pages/donations';
-import DonationsSucess from './pages/donations/success';
 
 function App() {
   return (
@@ -15,23 +11,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<CadastroUsuario />} />
         <Route path="/sucesso" element={<Success />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/donation"
-          element={
-            <PrivateRoute>
-              <Donations />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/donation-success"
-          element={
-            <PrivateRoute>
-              <DonationsSucess />
-            </PrivateRoute>
-          }
-        />
       </Routes>
     </Router>
   );
